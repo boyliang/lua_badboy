527abdf2d1d15c85843adece550565c0f9967339
boy liang <imcoffeeboy@foxmail.com>
2015-11-21 10:35:46 +0800

    修改main.lua的print为sysLog

--- main.lua
+++ main.lua
@@ -9,8 +9,8 @@ local c_d_balue = lua_value.c.d -- =1
 local raw_json_text = json.encode(lua_value)
 local pretty_json_text = json.encode_pretty(lua_value)
 
-print(raw_json_text)
-print(pretty_json_text)
+sysLog(raw_json_text)
+sysLog(pretty_json_text)
 
 
 local str = 'i am a badboy'
@@ -20,6 +20,6 @@ local hex = str:tohex() -- 等同于 strutils.tohex(str)
 local sha1 = str:sha1() -- 等同于 strutils.sha1(str)
 local md5 = str:md5() -- 等同于 strutils.md5(str)
 
-print('hex:' .. hex)
-print('sha1:' .. sha1)
-print('md5:' .. md5)
+sysLog('hex:' .. hex)
+sysLog('sha1:' .. sha1)
+sysLog('md5:' .. md5)
